e8e8bc8a064a1cee27b57e3da895b0a1730a0fd5
pauloismelo <seu-email@example.com>
2025-05-06 13:08:26 -0300

    feat: usercontroller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private readonly userService: UsersService) {}
+
+    @Get()
+    getAll(){
+        return '';
+    }
+
+    @Get(':id')
+    getOne(@Param('id') id: number){
+        return '';
+    }
+
+    @Post()
+    create(@Body() userData: {}){
+        return '';
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: number, @Body() userData: {}){
+        return '';
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: number){
+        return '';
+    }
+
+}
+
